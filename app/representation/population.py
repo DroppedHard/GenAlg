469914eb85b4fcc9363bd81ef5d2277fe938102f
xyz654 <maskrok@gmail.com>
2025-03-28 20:55:31 +0100

    Create best individuals

--- app/representation/population.py
+++ app/representation/population.py
@@ -3,7 +3,7 @@ from typing import Literal
 
 
 class Population:
-    def __init__(self, a:int, b:int, func, n_of_variables:int, chrom_length:int, population_size:int, epochs:int, optimization_type:Literal["min", "max"]):
+    def __init__(self, a:int, b:int, func, n_of_variables:int, chrom_length:int, population_size:int, epochs:int, optimization_type:Literal["min", "max"], best_indv_number: int):
         self.a = a
         self.b = b
         self.func = func
@@ -12,8 +12,8 @@ class Population:
         self.population_size = population_size
         self.epochs = epochs
         self.optimization_type = optimization_type
-        self.population = self.create_population()
-
+        self.best_indv_number = best_indv_number
+        self.population = self.create_population() 
 
     def create_population(self):
         return [Individual(self.a, self.b, self.func, self.chrom_length, self.n_of_variables) for _ in range(self.population_size)]
@@ -21,3 +21,16 @@ class Population:
     def sort_population(self):
         reverse_sort = self.optimization_type == "max"
         self.population = sorted(self.population, key=lambda ind: ind.target_function_val, reverse=reverse_sort)
+
+    def new_population(self, new_population):
+        self.population = new_population
+
+    def get_best_individuals(self):
+        '''
+        To be used after selection and setting new population to get the best individuals.
+        Warning: after this function is called, the population is sorted.
+        '''
+        self.sort_population()
+        self.elitary_population = self.population[:self.best_indv_number]
+        return self.elitary_population
+    
