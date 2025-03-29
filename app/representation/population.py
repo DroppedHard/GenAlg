9419bfc15419d6d190722794abcf7728d663725d
swoojcik <46939111+DroppedHard@users.noreply.github.com>
2025-03-29 17:13:20 +0100

    #11 - add GUI (#17)
    * Add new library, create sample main page
    * Fix labeled combo switching

--- app/representation/population.py
+++ app/representation/population.py
@@ -3,7 +3,18 @@ from typing import Literal
 
 
 class Population:
-    def __init__(self, a:int, b:int, func, n_of_variables:int, chrom_length:int, population_size:int, epochs:int, optimization_type:Literal["min", "max"], best_indv_number: int):
+    def __init__(
+        self,
+        a: int,
+        b: int,
+        func,
+        n_of_variables: int,
+        chrom_length: int,
+        population_size: int,
+        epochs: int,
+        optimization_type: Literal["min", "max"],
+        best_indv_number: int,
+    ):
         self.a = a
         self.b = b
         self.func = func
@@ -13,24 +24,32 @@ class Population:
         self.epochs = epochs
         self.optimization_type = optimization_type
         self.best_indv_number = best_indv_number
-        self.population = self.create_population() 
+        self.population = self.create_population()
 
     def create_population(self):
-        return [Individual(self.a, self.b, self.func, self.chrom_length, self.n_of_variables) for _ in range(self.population_size)]
-    
+        return [
+            Individual(
+                self.a, self.b, self.func, self.chrom_length, self.n_of_variables
+            )
+            for _ in range(self.population_size)
+        ]
+
     def sort_population(self):
         reverse_sort = self.optimization_type == "max"
-        self.population = sorted(self.population, key=lambda ind: ind.target_function_val, reverse=reverse_sort)
+        self.population = sorted(
+            self.population,
+            key=lambda ind: ind.target_function_val,
+            reverse=reverse_sort,
+        )
 
     def new_population(self, new_population):
         self.population = new_population
 
     def get_best_individuals(self):
-        '''
+        """
         To be used after selection and setting new population to get the best individuals.
         Warning: after this function is called, the population is sorted.
-        '''
+        """
         self.sort_population()
-        self.elitary_population = self.population[:self.best_indv_number]
+        self.elitary_population = self.population[: self.best_indv_number]
         return self.elitary_population
-    
